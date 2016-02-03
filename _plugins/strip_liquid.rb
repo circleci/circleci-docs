46e1d21911ecb850cb9534d26bccb59bacaaed38
Kevin Bell <kevin.a.bell@gmail.com>
2016-02-03 14:30:20 -0800

    Make docs a collection instead of normal pages

--- _plugins/strip_liquid.rb
+++
@@ -1,9 +0,0 @@
-module Jekyll
-  module StripLiquidFilter
-    def strip_liquid(input)
-      input.to_s.gsub(/\{%.*?%\}/, '')
-    end
-  end
-end
-
-Liquid::Template.register_filter(Jekyll::StripLiquidFilter)
