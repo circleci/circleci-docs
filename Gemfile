9d0f7e8b68ebf0709fd3d8d86181a7a3ebeab34d
Ricardo N Feliciano <FelicianoTech@gmail.com>
2016-03-16 15:32:34 -0700

    Updated environment for better CircleCI/Vagrant workflow.

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+
+ruby '2.3.0'
+
+gem 'jekyll'
+gem 'html-proofer'
--- bootstrap.sh
+++ bootstrap.sh
@@ -6,8 +6,9 @@ sudo apt-get -y install libgmp-dev git
 
 # Install RVM so we can run a recent version of Jekyll.
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-\curl -sSL https://get.rvm.io | bash -s stable --ruby=2.3.0
+\curl -sSL https://get.rvm.io | bash -s stable --ruby=2.3.0 --gems=bundler
 source ~/.rvm/scripts/rvm
 
-# Install Jekyll an any other gems. You can also swap this out for bundler.
-gem install jekyll
+# Install Jekyll and dependencies
+cd /vagrant
+bundle install
