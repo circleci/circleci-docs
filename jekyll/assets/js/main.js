4c6f3e47a64f30c57573094b96201d6c572c0acc
Ricardo N Feliciano <FelicianoTech@gmail.com>
2016-04-25 19:54:48 -0400

    Added anchor links to article headings.

--- jekyll/assets/js/main.js
+++ jekyll/assets/js/main.js
@@ -3,10 +3,19 @@
 
 $( document ).ready(function() {
 
+	// Allow navigation to slide open and close on small devices
 	$("#nav-button").click(function(){
 		event.preventDefault();
 
 		$("#nav-button").toggleClass("open");
 		$("nav.sidebar").toggleClass("open");
+	});
+
+	// Give article headings direct links to anchors
+	$("article h2, article h3, article h4, article h5, article h6").filter("[id]").each(function () {
+		$(this).append('<a href="#' + $(this).attr("id") + '"><i class="fa fa-link"></i></a>');
+	});
+	$("article h2, article h3, article h4, article h5, article h6").filter("[id]").hover(function () {
+		$(this).find("i").toggle();
 	});
 });
