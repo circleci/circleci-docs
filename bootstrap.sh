b31ca52a70bbbb6e9b03c8f0b6dc4aafb548cfcb
Ricardo N Feliciano <FelicianoTech@gmail.com>
2016-05-04 13:18:22 -0700

    Update toolset.

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,7 @@ sudo apt-get -y install libgmp-dev git
 
 # Install RVM so we can run a recent version of Jekyll.
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-\curl -sSL https://get.rvm.io | bash -s stable --ruby=2.3.0 --gems=bundler
+\curl -sSL https://get.rvm.io | bash -s stable --ruby=2.3.1 --gems=bundler
 source ~/.rvm/scripts/rvm
 
 # Install Jekyll and dependencies
