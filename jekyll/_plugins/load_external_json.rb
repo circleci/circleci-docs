87525671f5f5a33223571823d0e2f66232ae4942
ndintenfass <nathan@dintenfass.com>
2017-04-11 00:19:21 -0700

    clean up unused commented code

--- jekyll/_plugins/load_external_json.rb
+++ jekyll/_plugins/load_external_json.rb
@@ -1,23 +1,12 @@
 require 'json'
 require 'open-uri'
-
 module LoadExternalJSON
   class Generator < Jekyll::Generator
     safe true
     priority :highest
-
     def generate(site)
       if config = site.config['external_json']
         config.each do |source|
-          ## COMMENTING OUT THE BEHAVIOR OF MERGING EXISTING WITH FRESH - JUST GET IT EVERY TIME
-          # target = site.data[source['name']]
-          # source = JSON.load(open(source['url']))
-          # if target
-          #   HashJoiner.deep_merge target, source
-          # else
-          #   site.data[source['name']] = source
-          # end
-          # THIS NEXT LINE REPLACES THE ABOVE
           site.data[source['name']] = JSON.load(open(source['url']))
           if source['cache']
             data_source = (site.config['data_source'] || '_data')
