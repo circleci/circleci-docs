2e406012e8e7a95b9bda4c0423ae0bdf4dbd5db6
Trevor Sorel <trevor@circleci.com>
2018-08-29 13:52:41 -0700

    fixing sidebar error on large screens; fixing link color

--- jekyll/assets/js/sidebar.js
+++ jekyll/assets/js/sidebar.js
@@ -45,7 +45,7 @@
 
   function setSidebar () {
     // if footer is in frame, removed fixed style (otherwise add it, if it doesn't exist)
-    if ((footer.getBoundingClientRect().top - window.innerHeight) <= 0) {
+    if ((footer.getBoundingClientRect().top - window.innerHeight) <= 0 && footer.getBoundingClientRect().top >= window.innerHeight) {
       if (sidebar.classList.contains('fixed')) {
         sidebar.classList.remove('fixed');
       }
@@ -54,10 +54,18 @@
         sidebar.classList.add('fixed');
       }
     }
+
+    // prevents display problems on very large screens with little content
+    if (footer.getBoundingClientRect().top <= window.innerHeight) {
+      sidebar.style.height = (footer.getBoundingClientRect().top - 70) + 'px';
+    } else {
+      sidebar.style.height = null;
+    }
   };
 
   window.addEventListener('scroll', setSidebar);
   window.addEventListener('load', setSidebar);
+  window.addEventListener('resize', setSidebar);
 
   // allowing opening/closing of subnav elements
   var mainNavItems = Array.from(document.querySelectorAll('nav.sidebar .main-nav-item'));
