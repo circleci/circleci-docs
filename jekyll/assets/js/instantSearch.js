4d68d365d471b72ae442de1083c9b984f45fe9c9
Trevor Sorel <trevor@circleci.com>
2018-09-20 14:39:45 -0700

    adding instant search to dogs

--- jekyll/assets/js/instantSearch.js
+++ jekyll/assets/js/instantSearch.js
@@ -13,7 +13,7 @@ search.addWidget(
     cssClasses: {
       input: 'instantsearch-search'
     },
-    placeholder: 'Search for products'
+    placeholder: 'Search Documentation'
   })
 );
 
@@ -24,19 +24,48 @@ search.addWidget(
     cssClasses: {
       input: 'instantsearch-search'
     },
-    placeholder: 'Search for products'
+    placeholder: 'Search Documentation'
   })
 );
 
 // initialize hits widget
 search.addWidget(
   instantsearch.widgets.hits({
-    container: '#hits',
+    container: '#instant-hits',
     templates: {
       empty: 'No results',
-      item: '<a href="/docs/{{ url }}">{{ title }}</a>: {{ content }}'
+      item: '<a href="/docs{{ url }}"><div class="result-item-wrap"><strong>{{ title }}</strong><br><p>{{ content }}</p></div>'
     }
   })
 );
 
 search.start();
+
+(function () {
+  // insert search results
+  var searchResetButton = document.querySelector("#search-box .ais-search-box--reset");
+  var searchBox = document.querySelector("input.instantsearch-search");
+  var template = document.querySelector("#hits-template");
+  var resultContainer = document.querySelector('.main-body');
+  var resultDisplay = document.querySelector('.main-body');
+  var stateHolder = resultDisplay.cloneNode(true);
+
+  var renderResults = function (e) {
+    resultDisplay.innerHTML = "";
+    if (searchBox.value.length > 0) {
+      template.querySelector('#search-term-display').innerHTML = searchBox.value;
+      var results = template.cloneNode(true);
+
+      resultDisplay.appendChild(results);
+    } else {
+      resultDisplay.appendChild(stateHolder);
+    }
+  };
+
+  searchBox.addEventListener('keyup', renderResults);
+  window.addEventListener('load', renderResults);
+  searchResetButton.addEventListener('click', function () {
+    resultDisplay.innerHTML = "";
+    resultDisplay.appendChild(stateHolder);
+  });
+}());
