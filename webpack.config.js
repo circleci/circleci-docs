042c421f7d5ed15bdaf125590506e9e23f4f4c52
Juan Caicedo <retiredcanadianpoet@gmail.com>
2018-09-20 11:50:47 -0500

    html snippet for preparing page

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,28 @@
 const path = require('path');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
     app: './src-js/app.js'
   },
   output: {
-    path: path.join(__dirname, 'jekyll/assets/js/'),
-    filename: '[name].min.js'
+    // We need the flexibility to build a js file to one place and an html file
+    // in another, which are in different places in the jekyll directory tree.
+    // We set a fairly high level path and empty default path so that we can get
+    // this flexibility.
+    path: path.join(__dirname, 'jekyll/'),
+    publicPath: '',
+    filename: 'assets/js/dist/[name]-[contenthash].min.js'
   },
   plugins: [
-    new webpack.ProvidePlugin({
-      React: 'react',
-      ReactDOM: 'react-dom'
+    new HtmlWebpackPlugin({
+      filename: '_includes/app.html',
+      templateContent: ({ htmlWebpackPlugin }) =>
+        `<script src="{{ "/${
+          htmlWebpackPlugin.files.chunks.app.entry
+        }" | prepend: site.baseurl }}" type="text/javascript"></script>`,
+      inject: false
     })
   ],
   module: {
@@ -22,7 +32,7 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           query: {
-            presets: ['es2015', 'react']
+            presets: ['es2015']
           }
         }
       }
