37f56a6a47642fa3773ca59d1b8d125fae30a0c9
Rosie Yohannan <rosieyo@gmail.com>
2019-08-06 17:14:27 +0100

    remove rm step from script - not needed

--- scripts/build_pdfs.sh
+++ scripts/build_pdfs.sh
@@ -11,7 +11,6 @@ COMMENT="DRAFT"
 echo "Building Ops Guide"
 
 ### -- Build the Ops Guide --
-rm ${OUT_DIR}/CircleCI-Server-Operations-Guide.PDF
 asciidoctor-pdf -a pdf-style=jekyll/_cci2/release-pdf/circle-theme.yml -a pdf-fontsdir=jekyll/_cci2/release-pdf/fonts -a revdate=${DATE} -a revnumber=${VERSION} -a revremark=Draft -D ${OUT_DIR} -o CircleCI-Server-Operations-Guide.pdf jekyll/_cci2/_ops-guide.adoc
 
 echo "Done!"
@@ -19,7 +18,6 @@ echo "Done!"
 echo "Building AWS Install Guide"
 
 ### -- Build the AWS Install Guide --
-rm ${OUT_DIR}/CircleCI-Server-AWS-Installation-Guide.PDF
 asciidoctor-pdf -a pdf-style=jekyll/_cci2/release-pdf/circle-theme-install.yml -a pdf-fontsdir=jekyll/_cci2/release-pdf/fonts -a revdate=${DATE} -a revnumber=${VERSION} -a revremark=${COMMENT} -D ${OUT_DIR} -o CircleCI-Server-AWS-Installation-Guide.pdf jekyll/_cci2/_aws-install.adoc
 
 echo "Done!"
