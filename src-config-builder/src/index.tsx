7e35b1b9f12c643bd7c794194fdf26686e6e4f9e
teesloane <tylersloane@gmail.com>
2020-05-29 15:33:31 -0400

    Fix: use a tag component for GTM.

--- src-config-builder/src/index.tsx
+++ src-config-builder/src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import styled from "@emotion/styled";
+import TagManager from 'react-gtm-module'
 import {Sidebar, Docsbar, ConfigContent, Navbar} from "./components";
 import baseImg from './data/baseConfigs'
 import appConfig, {screens} from './config';
@@ -50,6 +51,7 @@ const App: React.FC = () => {
 };
 
 // Add google analyitcs.
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-W9HDVK');
+const tagManagerArgs = {gtmId: 'GTM-W9HDVK'}
+TagManager.initialize(tagManagerArgs)
 
 ReactDOM.render(<App />, document.getElementById("root"));
