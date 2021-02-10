cdf901642faea8491b7bb9ae239fc5a353004cb0
teesloane <tylersloane@gmail.com>
2021-02-10 11:01:40 -0500

    Clean: remove config-builder.

--- src-config-builder/src/components/index.tsx
+++
@@ -1,8 +0,0 @@
-export { Sidebar } from './Sidebar';
-export { Docsbar } from './Docsbar';
-export { Navbar } from './Navbar';
-export { ConfigContent } from './ConfigContent/';
-export { Icon } from './Icon/';
-export { Link } from './Link';
-export { default as Codemirror } from './Codemirror';
-export { H1, H3, P } from './Typography';
