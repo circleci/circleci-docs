7c7d2a1ece417b1c031c83a325a0edd47be152af
teesloane <tylersloane@gmail.com>
2021-02-19 13:48:15 -0500

    Add: sorting, printing.

--- scripts/docs-stats.py
+++ scripts/docs-stats.py
@@ -11,15 +11,23 @@ from pathlib import Path
 
 updated_docs_metadata = []
 
+docs_paths = (
+    '../jekyll/_cci2/*.md',
+    '../jekyll/_cci2/*.adoc',
+    '../jekyll/_cci2_ja/*.md',
+    '../jekyll/_cci2_ja/*.adoc',
+)
 
-def create_document_metadata(file_name, last_updated, last_updated_ts, file_path):
+def create_document_metadata(file_name, last_updated, last_updated_ts, file_path, file_dir):
     """Create a dict of metadata for each file; 
     a list of these dict are stored in updated_docs_metadata"""
     f = {
-       "last_updated": last_updated,
-       "last_updated_ts": last_updated_ts,
-       "file_name": file_name,
-       "file_path": file_path
+        "last_updated": last_updated,
+        "last_updated_ts": last_updated_ts,
+        "file_name": file_name,
+        "file_path": file_path,
+        "file_dir": file_dir
+
     }
     return f
 
@@ -27,20 +35,36 @@ def create_document_metadata(file_name, last_updated, last_updated_ts, file_path
 def iterate_docs():
     """retrieve data on each doc using git and store in a global list."""
     print("Checking last time updated...")
-    for file in list(glob.glob('../jekyll/_cci2/*.md')): # figure out why **/ glob is not working.
+    # for file in list(glob.glob('../jekyll/**/*.md')): # figure out why **/ glob is not working.
+    files = []
+    for path in docs_paths:
+        files.extend(glob.glob(path))
+
+
+    for file in files:
         relative_date = os.popen('git log -1 --date=relative --format="%ad" -- ' + file).read()
-        epoch_date    = os.popen('git log -1 --format="%ct"' + file).read()
-        file_name     = Path(file).stem
-        doc_data      = create_document_metadata(file_name, relative_date, epoch_date, file)
+        epoch_date    = os.popen('git log -1 --format="%ct" -- ' + file).read()
+        fpath         = Path(file)
+        file_name     = str(fpath.stem)
+        file_dir      = str(fpath.parent.name)
+        name          = file_dir + "/" + file_name + str(fpath.suffix)
+        doc_data      = create_document_metadata(name, relative_date, epoch_date, file, file_dir)
 
         updated_docs_metadata.append(doc_data)
 
 
 def print_docs_table():
-    for doc in updated_docs_metadata:
-        print ("{:<40} {:<20}".format(doc["file_name"], doc["last_updated"].rstrip()))
+    print ("{:<4} | {:<9} | {:<50} | {:<20}".format('#', 'Dir', 'File','Last Updated'))
+    print ("----------------------------------------------------------------------------------")
+    for index, doc in enumerate(updated_docs_metadata):
+        print ("{:<4} | {:<9} | {:<50} | {:<20}".format(index, doc['file_dir'], doc["file_name"], doc["last_updated"].rstrip()))
 
-## --
+def sort_by_date():
+    updated_docs_metadata.sort(key = lambda i: (i['file_dir'], i['last_updated_ts']))
+
+
+# -------
 
 iterate_docs()
+sort_by_date()
 print_docs_table()
