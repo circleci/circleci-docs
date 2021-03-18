4239f6368bce0602c3a8f718348b573cf5435daa
annabaker <anna.baker.313@gmail.com>
2021-03-18 01:36:52 -0400

    server 3 whats new

--- scripts/build_pdfs_asciidoc.sh
+++ scripts/build_pdfs_asciidoc.sh
@@ -4,20 +4,20 @@ OUT_DIR="release/tmp"
 
 DATE=$(date +"%m/%d/%Y")
 
-VERSION="2.19.12"
+VERSION="3.0.0"
 
 COMMENT="FINAL"
 
-echo "Building AWS Ops Guide"
+echo "Building Ops Guide"
 
 ### -- Build the Ops Guide --
 asciidoctor-pdf -a pdf-style=jekyll/_cci2/_release-pdf/circle-theme.yml -a pdf-fontsdir=jekyll/_cci2/_release-pdf/fonts -a skip-front-matter -a pdf=true -a revdate=${DATE} -a revnumber=${VERSION} -a revremark=${COMMENT} -a serverversion=${VERSION} -D ${OUT_DIR} -o CircleCI-Server-Operations-Guide.pdf jekyll/_cci2/_ops-guide.adoc
 
 echo "Done!"
 
-echo "Building AWS Install Guide"
+echo "Building Install Guide"
 
 ### -- Build the AWS Install Guide --
-asciidoctor-pdf -a pdf-style=jekyll/_cci2/_release-pdf/circle-theme-install.yml -a pdf-fontsdir=jekyll/_cci2/_release-pdf/fonts -a skip-front-matter -a pdf=true -a revdate=${DATE} -a revnumber=${VERSION} -a revremark=${COMMENT} -a serverversion=${VERSION} -D ${OUT_DIR} -o CircleCI-Server-AWS-Installation-Guide.pdf jekyll/_cci2/_aws-install.adoc
+asciidoctor-pdf -a pdf-style=jekyll/_cci2/_release-pdf/circle-theme-install.yml -a pdf-fontsdir=jekyll/_cci2/_release-pdf/fonts -a skip-front-matter -a pdf=true -a revdate=${DATE} -a revnumber=${VERSION} -a revremark=${COMMENT} -a serverversion=${VERSION} -D ${OUT_DIR} -o CircleCI-Server-Installation-Guide.pdf jekyll/_cci2/_install-guide.adoc
 
 echo "Done!"
