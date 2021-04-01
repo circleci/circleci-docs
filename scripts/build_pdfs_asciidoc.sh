056b9907a554e551e19174c3f6908f0652351eaf
annabaker <anna.baker.313@gmail.com>
2021-04-01 15:35:11 -0400

    Testing pdf gen and sidebar

--- scripts/build_pdfs_asciidoc.sh
+++ scripts/build_pdfs_asciidoc.sh
@@ -19,6 +19,6 @@ asciidoctor-pdf -a pdf-style=jekyll/_cci2/_release-pdf/circle-theme.yml -a pdf-f
 echo "Done!"
 
 # Build Install Guide
-echo "Building Server Installation Guide"
+echo "Building Server Installation Guide..."
 asciidoctor-pdf -a pdf-style=jekyll/_cci2/_release-pdf/circle-theme-install.yml -a pdf-fontsdir=jekyll/_cci2/_release-pdf/fonts -a skip-front-matter -a pdf=true -a revdate=${DATE} -a revnumber=${VERSION} -a revremark=${COMMENT} -a serverversion=${VERSION} -D ${OUT_DIR} -o CircleCI-Server-${VERSION}-Installation-Guide.pdf jekyll/_cci2/_server-3-install-guide.adoc
 echo "Done!"
