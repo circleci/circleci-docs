490114f5ae7afb3e6a362d07aac3b0e1cb6e14f9
teesloane <tylersloane@gmail.com>
2021-04-13 11:58:33 -0400

    Enable reading from custom file + cleaning.

--- scripts/block_list.sh
+++ scripts/block_list.sh
@@ -3,21 +3,39 @@ set -euo pipefail
 
 # gather the list of words that are on the bocklist and join them together to be
 # used as a regex.
-export WORD_LIST=$(cat words.txt | tr -s '[:space:]' '|' | sed 's/|$//g')
-
-# run a git diff on all changed words, and grep for anything that might be on WORD_LIST
-git diff --word-diff | grep -E '\{\+('${WORD_LIST}')\+\}'
 
 
-if [ $? = 0 ]; then exit 1; fi
+export WORD_LIST=$(cat ${BLOCKED_WORDS:-words.txt} | tr -s '[:space:]' '|' | sed 's/|$//g')
 
-# Next steps
 
-# have a flag to print warning instead of failing
-# (also, build a "failure"), right now it's just echoing "FAILURE" -> should exit N
-# status quo: run an exit with the last command's output ?
+# search proposed changes for use of blocked words.
+search_git_diff() {
+    # TODO: can we make the grep case-insensitive?
+    git diff --word-diff | grep -E '\{\+('${WORD_LIST}')\+\}'
+
+    if [ $? = 0 ] && [ ${DISABLE_WORD_BLOCKLIST:-false} == "true" ]
+    then
+        echo "\n⚠️ Warning - Blocklist words found in changes."
+        exit 0
+    else
+        echo "\n❌ Failing - Blocklist words found in changes."
+        exit 1
+    fi
+}
+
+
+# search existing content for blocked words.
+# search_all() {
+#     echo "Searching entire folder for blocked words..."
+#     # TODO
+# }
+
+
+echo "Searching for occurrences of blocked words in changes...\n"
+search_git_diff
+
 
 ## Possible options / flags
-
 # - warn on existing content
 # - enable feeding a custom word list into the file
+
