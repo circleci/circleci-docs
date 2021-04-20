c3461db4c180a590c70471ccb7b2ca34c8f23396
Todd Eichel <todd@toddeichel.com>
2021-04-20 14:27:56 -0700

    Clean up previous version of nav/header code

--- jekyll/assets/js/nav.js
+++ jekyll/assets/js/nav.js
@@ -12,9 +12,6 @@ $(document).ready(function () {
 (function () {
   // grabbing all expandable submenus
   var expandableSubMenus = Array.from(document.querySelectorAll('nav li.arrow'));
-  var mobileMenuBtn = document.querySelectorAll('button.navbar-toggle')[0];
-  var htmlBody = document.getElementsByTagName('body')[0];
-  var mobileNav = document.getElementById('mobile-navigation');
 
   expandableSubMenus.forEach(function (submenu) {
     submenu.addEventListener('click', function () {
@@ -24,30 +21,6 @@ $(document).ready(function () {
         this.classList.add('collapsed');
       }
     });
-  });
-
-  var preventDefaultTouch = function (e) {
-    e.preventDefault();
-  };
-
-  // on click of nav, on mobile, hide body scroll, to make only nav scrollable
-  mobileMenuBtn.addEventListener('click', function (btnEvt) {
-    if (btnEvt.target.classList.contains('collapsed')) {
-      htmlBody.style.overflow = "hidden";
-      document.addEventListener('touchmove', preventDefaultTouch, { passive: false });
-
-      var offset, navEvt;
-      mobileNav.addEventListener('touchstart', function (evt) {
-        offset = this.scrollTop; 
-        navEvt = evt;
-      });
-      mobileNav.addEventListener('touchmove', function (navEvt2) {
-        this.scrollTop = (navEvt.touches[0].clientY - navEvt2.touches[0].clientY) + offset;
-      });
-    } else {
-      htmlBody.style.overflow = "initial";
-      document.removeEventListener('touchmove', preventDefaultTouch, { passive: false });
-    }
   });
 
   // Open dropdowns on keyboard focus
