f653e74ad869704785307323178ee688da9d2ffb
Todd Eichel <todd@toddeichel.com>
2021-04-29 18:54:08 -0700

    Add search cancel button on mobile (desktop pending)
    This disentangles the two functions formerly served by the X button: 1)
    clearing the query, 2) exiting the search mode (and hiding the results
    display elements).
    
    This leaves the same issue unsolved on desktop until we have a design
    for the cancel button in that context, but it needs a similar treatment.

--- src-js/instantsearch.js
+++ src-js/instantsearch.js
@@ -191,10 +191,23 @@ export function init () {
 
   // insert search results
   var searchResetButton = document.querySelector("#search-box .ais-SearchBox-reset");
+  var searchCancelButton = document.querySelector(".main-searchbar--cancel-button");
   var searchBox = document.querySelector("input.instantsearch-search");
   var documentBody = document.querySelector('body');
   var resultDisplay = document.querySelector('#hits-target');
   var form = document.querySelector('.main-searchbar form');
+
+  function clearResults() {
+    // Clear hit counts
+    Array.from(resultDisplay.querySelectorAll('.hits-count-counter')).forEach(function (el) {
+      el.innerText = el.dataset.initialValue;
+    });
+
+    // Clear results
+    Array.from(resultDisplay.querySelectorAll('.instant-hits')).forEach(function (el) {
+      el.innerHTML = '';
+    });
+  };
 
   function renderResults (e) {
     if (searchBox.value.length > 0) {
@@ -202,9 +215,16 @@ export function init () {
       documentBody.classList.add('search-open');
       window.dispatchEvent(new Event('shown.subnav'));
     } else {
-      documentBody.classList.remove('search-open');
+      clearResults();
     }
   };
+
+  function resetSearch() {
+    // Reset by triggering Instantsearch's own reset button, which also removes
+    // the search query from the URL query string
+    searchResetButton.click();
+    renderResults();
+  }
 
   searchBox.addEventListener('keyup', renderResults);
 
@@ -214,11 +234,13 @@ export function init () {
   });
 
   window.addEventListener('load', renderResults);
-  searchResetButton.addEventListener('click', function (e) {
+
+  searchResetButton.addEventListener('click', resetSearch);
+
+  searchCancelButton.addEventListener('click', function (e) {
     e.preventDefault();
-    searchBox.value = ''
-    searchBox.blur();
-    renderResults();
+    resetSearch();
+    documentBody.classList.remove('search-open');
   });
 
   // Scroll results back to top upon tab change
