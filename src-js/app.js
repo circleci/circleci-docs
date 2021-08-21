3b34f5b17551da798599e3e474d3b5a3483b8757
rsylvian <romain.sylvian@gmail.com>
2021-08-20 17:03:40 -0700

    first optimizely integration

--- src-js/app.js
+++ src-js/app.js
@@ -2,6 +2,16 @@ import * as Cookie from 'js-cookie';
 import * as search from './instantsearch.js';
 import * as lang from './lang.js'
 
+const optimizely = require('@optimizely/optimizely-sdk');
+
 window.Cookie = Cookie;
 search.init();
 lang.init();
+
+var optimizelyClient = optimizely.createInstance({
+  datafile: window.optimizelyDatafile,
+});
+
+optimizelyClient.onReady().then(() => {
+  console.log("Optimizely Ready:):)", window.userData);
+});
