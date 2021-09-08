0350acd40d04d388db4200b337fa91f9c0090847
rsylvian <romain.sylvian@gmail.com>
2021-09-07 17:44:34 -0700

    add comments

--- src-js/app.js
+++ src-js/app.js
@@ -4,12 +4,13 @@ import * as lang from './lang.js'
 import OptimizelyClient from './optimizely.js';
 import AnalyticsClient from "./analytics.js";
 
+ // imported but not used just so webpack picks it up and add it to the `app.bundle.js`
 import * as highlightJSBadge from 'highlightjs-badge';
 
-window.Cookie = Cookie;
 search.init();
 lang.init();
 
+// adding "Clients" to the window object so they can be accessed by other js inside Jekyll
+window.Cookie = Cookie;
 window.OptimizelyClient = new OptimizelyClient();
-window.AnalyticsClient = AnalyticsClient;
-
+window.AnalyticsClient = AnalyticsClient; // because it only has static methods, AnalyticsClient is not instantiated
