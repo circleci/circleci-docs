ba7a9905fcf18b4d7ebd8df9a1f7d7a25b7c24e7
Romain Sylvian <romain@circleci.com>
2021-09-07 17:48:39 -0700

    Add AnalyticsClient to have a thin wrapper around Segment/Amplitude Analytics (#5662)
    * rename optimizely client file
    
    * add an AnalyticsClient
    
    * add trackAction
    
    * Update event name for docs-copy-code-clicked
    
    * add comments

--- src-js/app.js
+++ src-js/app.js
@@ -1,11 +1,16 @@
 import * as Cookie from 'js-cookie';
 import * as search from './instantsearch.js';
 import * as lang from './lang.js'
-import OptimizelyClient from './optimizelyClient.js';
+import OptimizelyClient from './optimizely.js';
+import AnalyticsClient from "./analytics.js";
+
+ // imported but not used just so webpack picks it up and add it to the `app.bundle.js`
 import * as highlightJSBadge from 'highlightjs-badge';
 
-window.Cookie = Cookie;
 search.init();
 lang.init();
 
+// adding "Clients" to the window object so they can be accessed by other js inside Jekyll
+window.Cookie = Cookie;
 window.OptimizelyClient = new OptimizelyClient();
+window.AnalyticsClient = AnalyticsClient; // because it only has static methods, AnalyticsClient is not instantiated
