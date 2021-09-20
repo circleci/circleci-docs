76d0afa4bdd294aa6449d85a77518e43c1a1b994
rsylvian <romain.sylvian@gmail.com>
2021-09-20 12:20:26 -0700

    add tracking

--- src-js/experiments/relatedResources.js
+++ src-js/experiments/relatedResources.js
@@ -1,26 +1,19 @@
 $(() => {
-  // https://app.optimizely.com/v2/projects/16812830475/experiments/20598037463/variations
+  // https://app.optimizely.com/v2/projects/16812830475/experiments/20638820544/variations
   window.OptimizelyClient.getVariationName({
     experimentKey: 'dd_docs_related_resources_mvp_test',
     groupExperimentName: 'q3_fy22_docs_disco_experiment_group_test'
   }).then(variation => {
     if (variation === "treatment") {
       $('#related-resources').show();
-
+      // track clicks on each items
       $("#related-resources li a").click(function () {
-        console.log({
+        window.AnalyticsClient.trackAction('related-rerouces-clicked', {
           page: location.pathname,
           link: $(this).attr('href'),
           name: $(this).text()
         });
-
-        // window.AnalyticsClient.trackAction('related-rerouces-clicked', {
-        //   page: location.pathname,
-        //   link: $(this).attr('href'),
-        //   name: $(this).text()
-        // });
       });
-      //$('#related-resources li').click(())
     }
   })
 })
