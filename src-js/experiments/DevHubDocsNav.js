b09a734f4cab933627363aa8f87425356bd8ac36
teesloane <tylersloane@gmail.com>
2021-09-20 15:02:11 -0400

    Add: experiment code; cleanup.

---
+++ src-js/experiments/DevHubDocsNav.js
@@ -0,0 +1,17 @@
+
+$(() => {
+  // https://app.optimizely.com/v2/projects/16812830475/experiments/20616121296/variations
+  window.OptimizelyClient.getVariationName({
+    experimentKey: 'dd_merge_devhub_docs_experiment_test',
+    groupExperimentName: 'q3_fy22_docs_disco_experiment_group_test'
+  }).then(variation => {
+    if (variation === "treatment") {
+      $(".dd-global-nav--links").show();
+      $(".dd-devhub-docs-nav-link").click(function(event) {
+        event.preventDefault();
+        window.AnalyticsClient.trackAction('docs-devhub-nav-merge-link-clicked', {link: this.href})
+        window.location = this.href
+      })
+    }
+  })
+})
