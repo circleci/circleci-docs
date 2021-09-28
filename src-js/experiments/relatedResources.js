82d324b1e7c71537c86e8b9bddc2f7076d184f11
rsylvian <romain.sylvian@gmail.com>
2021-09-27 18:04:48 -0700

    audience optimizely link

--- src-js/experiments/relatedResources.js
+++ src-js/experiments/relatedResources.js
@@ -4,6 +4,7 @@ $(() => {
     experimentKey: 'dd_docs_related_resources_mvp_test',
     groupExperimentName: 'q3_fy22_docs_disco_experiment_group_test',
     attributes: {
+      // https://app.optimizely.com/v2/projects/16812830475/audiences/20680670160
       windowWidth: window.innerWidth ?? 0
     }
   }).then(variation => {
