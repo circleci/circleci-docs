bbe8096bcdcd87b22657b17982fbc890bf80bab6
rsylvian <romain.sylvian@gmail.com>
2021-10-04 12:47:37 -0700

    address feedback

--- src-js/experiments/DevHubDocsNav.js
+++ src-js/experiments/DevHubDocsNav.js
@@ -6,26 +6,16 @@ $(() => {
     groupExperimentName: 'q3_fy22_docs_disco_experiment_group_test',
     attributes: {
       // https://app.optimizely.com/v2/projects/16812830475/audiences/20678820396/#modal
-      windowWidth: window.innerWidth ?? 0, 
+      windowWidth: window.innerWidth ?? 0,
     }
   }).then(variation => {
     if (variation === "treatment") {
-      // TODO: Fig1 - PR not done until the below section replaces this:
-      // $(".dd-global-nav--links").show();
-      // $(".dd-global-nav--links a").click(function(_) {
-      //   window.AnalyticsClient.trackAction('docs-devhub-nav-merge-link-clicked', {link: this.href})
-      // })
+      $(".global-nav--component").addClass("dd-global-nav--component")
+
+      $(".dd-global-nav--links").show();
+      $(".dd-global-nav--links a").click(function(_) {
+        window.AnalyticsClient.trackAction('docs-devhub-nav-merge-link-clicked', {link: this.href})
+      })
     }
   })
-})
-
-// this code below is a shim because i'm not in docker:
-// TODO: PR not done until the below repalces --Fig1 above
-
-// Add a global experiment class, so that the nested scss auto applies styles.
-$(".global-nav--component").addClass("dd-global-nav--component")
-
-$(".dd-global-nav--links").show();
-$(".dd-global-nav--links a").click(function(_) {
-  window.AnalyticsClient.trackAction('docs-devhub-nav-merge-link-clicked', {link: this.href})
-})
+});
