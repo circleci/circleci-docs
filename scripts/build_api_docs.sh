ce111543378a8ee306bc7ea5b274aab2f538ba88
teesloane <tylersloane@gmail.com>
2021-10-13 15:02:34 -0400

    Add: template file with analytics code to api v2.
    The redoc-cli enables the use of a custom handlebar template;
    using this we are able to attach our analytics snippet to the head of
    the document before it is rendered.

--- scripts/build_api_docs.sh
+++ scripts/build_api_docs.sh
@@ -18,9 +18,9 @@ build_api_v1() {
 
 # We build our API v2 documentation from an openAPI spec. After fetching the spec we:
 # a) augment the spec using "snippet-enricher-cli" to add code-samples to the spec.
-# b) TODO: merge in any custom code samples we need to alter, using jq.
-# b) run the spec through redoc-cli, outputting a single html file.
-# c) move the file into our temporary workspace, created in .circleci/config.yml
+# b) merge in any custom code samples we need to alter, using jq.
+# c) run the spec through redoc-cli, outputting a single html file.
+# d) move the file into our temporary workspace, created in .circleci/config.yml
 build_api_v2() {
     echo "Building API v2 documentation with Redoc"
     cd src-api;
@@ -31,20 +31,10 @@ build_api_v2() {
     echo "Merging in JSON patches to correct and augment the OpenAPI spec."
     jq -s '.[0] * .[1]' openapi-with-examples.json openapi-patch.json > openapi-final.json
     echo "Bundling with redoc cli."
-    ./node_modules/.bin/redoc-cli bundle openapi-final.json
+    ./node_modules/.bin/redoc-cli bundle openapi-final.json --template "../src-api/v2/template.hbs"
     echo "Moving build redoc file to api/v2"
     mv redoc-static.html index.html
     cp index.html /tmp/workspace/api/v2
-}
-
-# build the Config Reference from slate.
-build_crg() {
-    echo "Building Configuration Reference with Slate "
-    cd src-crg;
-    bundle exec middleman build --clean --verbose
-    echo "CRG bundle built."
-    cp -R build/* /tmp/workspace/crg
-    echo "CRG Output build moved to /tmp/workspace/crg"
 }
 
 
@@ -54,9 +44,6 @@ then
 elif [ "$1" == "-v2" ]
 then
 	build_api_v2
-elif [ "$1" == "-crg" ]
-then
-	build_crg
 else
 	echo "Invalid command"
 fi
