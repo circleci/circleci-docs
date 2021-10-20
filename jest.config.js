ad4f6650b51286c72ad25bfd0c07057c8f796050
rsylvian <romain.sylvian@gmail.com>
2021-10-19 23:24:55 -0700

    add jest tests for services/analytics.js

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  testEnvironment: "jsdom",
+  // coverageThreshold: {
+  //   global: {
+  //     branches: 73.4,
+  //     functions: 81.43,
+  //     lines: 86,
+  //     statements: 85.5,
+  //   },
+  // },
+};
