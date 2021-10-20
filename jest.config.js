60d05a377424318229f1a1d3efd365b416739423
rsylvian <romain.sylvian@gmail.com>
2021-10-19 23:27:49 -0700

    cleanup

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,3 @@
 module.exports = {
   testEnvironment: "jsdom",
-  // coverageThreshold: {
-  //   global: {
-  //     branches: 73.4,
-  //     functions: 81.43,
-  //     lines: 86,
-  //     statements: 85.5,
-  //   },
-  // },
 };
