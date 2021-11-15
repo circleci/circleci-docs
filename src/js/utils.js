85021b02500db03693e1c1d16041914129158b6b
teesloane <tylersloane@gmail.com>
2021-11-15 13:40:05 -0500

    Fix: remove extra headlines when collecting.
    - on click - set active.
    - in prgoress: get first item on screen - add active class.

--- src/js/utils.js
+++ src/js/utils.js
@@ -8,3 +8,24 @@ export function isUnsupportedBrowser() {
   const msie = ua.indexOf('MSIE ');
   return msie > 0;
 }
+
+/**
+ * checks if an item is in the viewport
+ * shamelessly borrowed from https://stackoverflow.com/a/7557433
+ * */
+export function isElementInViewport(el) {
+  // Special bonus for those using jQuery
+  if (typeof jQuery === 'function' && el instanceof jQuery) {
+    el = el[0];
+  }
+
+  var rect = el.getBoundingClientRect();
+
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
