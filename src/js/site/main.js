6c37aa156665b16c2ec92d0a1bd10a1ab7ee2869
teesloane <tylersloane@gmail.com>
2021-11-15 14:31:54 -0500

    Fix: add missing experimentContainer.

--- src/js/site/main.js
+++ src/js/site/main.js
@@ -346,6 +346,7 @@ $(document).ready(function () {
   window.OptimizelyClient.getVariationName({
     experimentKey: 'dd_share_section_icon_test',
     groupExperimentName: 'q3_fy22_docs_disco_experiment_group_test',
+    experimentContainer: '.article-container',
   }).then((variation) => {
     if (variation === 'treatment') {
       document.querySelectorAll('.fa-link').forEach((icon) => {
