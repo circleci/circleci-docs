617cba4cd5b0fb7c1477358da7c823c6efe30b77
teesloane <tylersloane@gmail.com>
2021-11-18 15:53:22 -0500

    Add: more tests.

--- src/js/site/sidebar.test.js
+++ src/js/site/sidebar.test.js
@@ -7,39 +7,72 @@ describe('Sidebar', () => {
   });
 
   describe('highlightTocOnScroll', () => {
+    let h2, tocItem;
+
     beforeEach(() => {
+
       jest.resetAllMocks();
+      h2 = document.createElement('h2');
+      tocItem = document.createElement('a');
+
+      jest
+        .spyOn(document, 'querySelectorAll')
+        .mockImplementation((selector) => {
+          switch (selector) {
+            case '.toc-entry a':
+              let queriedTocItems = document.createDocumentFragment();
+              queriedTocItems.appendChild(tocItem);
+              return queriedTocItems.childNodes;
+            case 'h2, h3, h4, h5, h6':
+              let queriedHeadings = document.createDocumentFragment();
+              queriedHeadings.appendChild(h2);
+              return queriedHeadings.childNodes;
+          }
+        });
     });
 
     it('Calls observe', () => {
       // because InteractionObserver is a class on the window we need to actually
       // spy on the prototype of the method.
       const spy = jest.spyOn(IntersectionObserver.prototype, 'observe');
-      let h2 = document.createElement('h2');
-      let tocItem = document.createElement('a');
-
-      jest
-        .spyOn(document, 'querySelectorAll')
-        .mockImplementation((selector) => {
-          switch (selector) {
-            case '.toc-entry a':
-              let queriedTocItems = document.createDocumentFragment();
-              queriedTocItems.appendChild(tocItem);
-              return queriedTocItems.childNodes;
-            case 'h2, h3, h4, h5, h6':
-              let queriedHeadings = document.createDocumentFragment();
-              queriedHeadings.appendChild(h2);
-              return queriedHeadings.childNodes;
-          }
-        });
-
       sidebar.highlightTocOnScroll();
       expect(spy).toHaveBeenCalled();
     });
 
-    it("A document with one h2 is in the TOC and is 'active'", () => {
-      let h2 = document.createElement('h2');
+    it('Does not call observe if there are no titles.', () => {
+      const spy = jest.spyOn(IntersectionObserver.prototype, 'observe');
       let tocItem = document.createElement('a');
+
+      // we override the jest query selector here because
+      // we need to mock the case where there are not titles.
+      jest
+        .spyOn(document, 'querySelectorAll')
+        .mockImplementation((selector) => {
+          switch (selector) {
+            case '.toc-entry a':
+              let queriedTocItems = document.createDocumentFragment();
+              queriedTocItems.appendChild(tocItem);
+              return queriedTocItems.childNodes;
+            case 'h2, h3, h4, h5, h6':
+              let queriedHeadings = document.createDocumentFragment();
+              return queriedHeadings.childNodes;
+          }
+        });
+
+      sidebar.highlightTocOnScroll();
+      expect(spy).not.toHaveBeenCalled();
+    });
+
+    it("A document with one h2 is in the TOC and is 'active'", () => {
+      sidebar.highlightTocOnScroll();
+      expect([...tocItem.classList]).toEqual(
+        expect.arrayContaining(['active']),
+      );
+    });
+
+    it('Does not add an active class to TOC item when a ignored headline is encountered', () => {
+      let h2 = document.createElement('h2');
+      h2.classList.add('help-improve-header');
 
       jest
         .spyOn(document, 'querySelectorAll')
@@ -57,9 +90,9 @@ describe('Sidebar', () => {
         });
 
       sidebar.highlightTocOnScroll();
-      let x = [...tocItem.classList];
+
       expect([...tocItem.classList]).toEqual(
-        expect.arrayContaining(['active']),
+        expect.not.arrayContaining(['active']),
       );
     });
   });
