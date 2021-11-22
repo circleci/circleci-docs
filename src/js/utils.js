29fa6afb1e2c0671329810eaf51ea7336feaa4f2
tees <12987958+teesloane@users.noreply.github.com>
2021-11-22 14:19:34 -0500

    Revert "[CIRCLE-38942] - TOC highlight as you scroll." (#5962)
    * Revert "[CIRCLE-38942] - TOC highlight as you scroll. (#5924)"
    
    This reverts commit 8d2731a0ea181c3502fa763487033d106522a5dc.
    
    * add back class to disable ja codeowner req

--- src/js/utils.js
+++ src/js/utils.js
@@ -8,27 +8,3 @@ export function isUnsupportedBrowser() {
   const msie = ua.indexOf('MSIE ');
   return msie > 0;
 }
-
-/**
- * checks if an item is in the viewport
- * shamelessly borrowed from https://stackoverflow.com/a/7557433
- * */
-export function isElementInViewport(el) {
-  if (!(el instanceof HTMLElement)) {
-    return false;
-  }
-
-  // Special bonus for those using jQuery
-  if (typeof jQuery === 'function' && el instanceof jQuery) {
-    el = el[0];
-  }
-  var rect = el.getBoundingClientRect();
-
-  return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <=
-      (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-  );
-}
