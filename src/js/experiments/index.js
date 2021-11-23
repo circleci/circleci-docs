07d5110d35df8c19f6356e2fb3ae6e7d29dbc487
rsylvian <romain.sylvian@gmail.com>
2021-11-22 16:56:31 -0800

    add indexes with import/export

---
+++ src/js/experiments/index.js
@@ -0,0 +1,15 @@
+import DevHubDocsNav from './DevhubDocsNav';
+import forceAll from './forceAll';
+import highlightjsBadge from 'highlightjs-badge';
+import relatedResources from './relatedResources';
+import surfaceFullExampleConfig from './surfaceFullExampleConfig';
+import videoTutorials from './videoTutorials';
+
+export default {
+  DevHubDocsNav,
+  forceAll,
+  highlightjsBadge,
+  relatedResources,
+  surfaceFullExampleConfig,
+  videoTutorials,
+};
---
+++ src/js/services/index.js
@@ -0,0 +1,7 @@
+import AnalyticsClient from './analytics';
+import * as instantsearch from './instantsearch';
+import * as lang from './lang';
+import OptimizelyClient from './optimizely';
+import * as rum from './rum';
+
+export default { AnalyticsClient, instantsearch, lang, OptimizelyClient, rum };
