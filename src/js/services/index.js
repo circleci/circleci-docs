e772b1cc8101d338a90a3b833d801bf3ab430f4b
nav <nramabad@gmail.com>
2021-11-23 17:16:57 -0800

    Feat: promote copy code btn experiment and improve design

--- src/js/services/index.js
+++ src/js/services/index.js
@@ -3,5 +3,13 @@ import * as instantsearch from './instantsearch';
 import * as lang from './lang';
 import OptimizelyClient from './optimizely';
 import * as rum from './rum';
+import highlightjsBadge from '../services/highlightJSBadge';
 
-export default { AnalyticsClient, instantsearch, lang, OptimizelyClient, rum };
+export default {
+  AnalyticsClient,
+  instantsearch,
+  lang,
+  OptimizelyClient,
+  rum,
+  highlightjsBadge,
+};
