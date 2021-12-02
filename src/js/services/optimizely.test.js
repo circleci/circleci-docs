9e5f90864bd875f64dd8761a105c40f7114f09f0
chantelle g <chantelle@circleci.com>
2021-12-02 08:28:03 -0500

    notes

--- src/js/services/optimizely.test.js
+++ src/js/services/optimizely.test.js
@@ -1,7 +1,16 @@
-// import OptimizelyClient from './optimizely';
+import { optimize } from 'webpack';
+import OptimizelyClient from './optimizely';
 // import * as optimizelySDK from '@optimizely/optimizely-sdk';
 // import { default as CookieOrginal } from 'js-cookie';
 // import glob from '../../../jest/global';
+
+
+/*Todo:
+Setup mock file for promise async
+Figure out localstorage syntax
+*/
+
+
 
 jest.mock('js-cookie');
 // const Cookie = CookieOrginal;
@@ -12,5 +21,87 @@ describe('Optimizely Service', () => {
   });
   it('true is true', () => {
     expect(true).toBe(true);
+  });
+
+  describe('getUserId()', () => {
+    //***Need to create a mock file for async promise
+
+    it('test if userData already exists', () => {
+    
+    });
+
+    it('test waiting for userData to populate', () => {
+    
+    });
+
+    //Should we add a no userData test?
+
+  });
+
+  //Function used in TrackExperimentViewed
+  describe('isExperimentAlreadyViewed()', () => {
+    it('should be called with options object', () => {
+      //jest.spyOn(localStorage, "getItem");
+      //localStorage.getItem = jest.fn();
+      //expect(localStorage.getItem).toHaveBeenCalled();
+    });
+  });
+
+  //Function used in TrackExperimentViewed
+  describe('storeExperimentParticipation()', () => {
+    it('test localStorage key', () => {
+      //jest.spyOn(localStorage, "getItem");
+      //localStorage.getItem = jest.fn();
+      //expect(localStorage.getItem).toHaveBeenCalled();
+    });
+
+    it('test experiments value', () => {
+
+    });
+
+    it('test set localStorage key', () => {
+
+    });
+
+  });
+
+  //Function used in getVariationName
+  describe('trackExperimentViewed()', () => {
+    it('check experimentContainer', () => {
+
+    });
+
+    it('check isExperimentAlreadyViewed on experimentKey', () => {
+
+    });
+
+    it('set trackAction Experiment Viewed properties', () => {
+
+    });
+
+    it('store ExperimentParticipation using function', () => {
+
+    });
+
+  });
+
+  describe('getVariationName()', () => {
+    //***Need to create a mock file for async promise
+
+    it('test parameters are sent', () => {
+    
+    });
+
+    it('test orgId from cookie', () => {
+    
+    });
+
+    it('test isInGrowthExperimentGroup', () => {
+    
+    });
+
+    it('test tackExperimentViewed data', () => {
+    
+    });
   });
 });
