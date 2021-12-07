27288d0e80302c76e3b82e5e3a3bffe261bda4ba
Tony <tshen815@gmail.com>
2021-12-07 15:26:03 -0800

    add .sh script to minimize images

---
+++ scripts/process_images.sh
@@ -0,0 +1,30 @@
+
+#!/bin/bash 
+process_images_recurse() {
+    for i in "$1"/*;do # gives first level of dir
+        if [ -d "$i" ];then # for loop, -d is special flag that corresponds to dir
+            process_images_recurse "$i"
+        elif [ -f "$i" ]; then # else statement, -f corresponds to file 
+            echo "processing: $i"
+            mv $i $i.__tmp__
+            imagemin $i.__tmp__ > $i # > means take .__tmp image and replace with the new compressed image 
+            rm $i.__tmp__
+        fi
+    done
+}
+
+# conditional statement in shell
+# $# refers to length of all the arguments 
+# -ne 1 checks if the arguments will be one 
+# [[ $# -ne 1 ]] will evaluate into either true or false 
+if [ ! -d "$1" ]; then
+    echo "cant find directory"
+fi
+
+if [[ $# -ne 1 ]]; then 
+    echo "Usage: $0 <input folder>" # sh version of console log 
+    exit 2 # exit code: refers to an error for misuse of shell syntax 
+fi # end block of an if statement 
+
+# invoking here with one argument (first argument you pass)
+process_images_recurse $1
