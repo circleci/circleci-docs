0bd4de0f441cfd0c5c572563cf21dd6b0758d64e
chantelle g <chantelle@circleci.com>
2021-12-13 22:37:49 -0500

    updated X days ago

--- src/js/site/main.js
+++ src/js/site/main.js
@@ -402,3 +402,42 @@ $(document).ready(function () {
 
 // Currently this function is only used for the insights table
 $(document).ready(highlightURLHash());
+
+// update date shown to be X ago
+$(document).ready(function () {
+  function ago(date) {
+    function render(n, unit) {
+      return n + ' ' + unit + (n == 1 ? '' : 's') + ' ago';
+    }
+
+    var seconds = Math.floor((new Date() - date) / 1000);
+
+    var interval = Math.floor(seconds / (60 * 60 * 24 * 365));
+    if (Math.floor(seconds / (60 * 60 * 24 * 30 * 365)) >= 1) {
+      return render(interval, 'year');
+    }
+    interval = Math.floor(seconds / (60 * 60 * 24 * 30));
+    if (interval >= 1) {
+      return render(interval, 'month');
+    }
+    interval = Math.floor(seconds / (60 * 60 * 24));
+    if (interval >= 1) {
+      return render(interval, 'day');
+    }
+    interval = Math.floor(seconds / (60 * 60));
+    if (interval >= 1) {
+      return render(interval, 'hour');
+    }
+    interval = Math.floor(seconds / 60);
+    if (interval >= 1) {
+      return render(interval, 'minute');
+    }
+    interval = Math.floor(seconds);
+    return render(interval, 'second');
+  }
+
+  var date = Date.parse(
+    document.getElementById('postedon').getAttribute('datetime'),
+  );
+  document.getElementById('postedago').innerHTML = ago(date);
+});
