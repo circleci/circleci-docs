29c6a2155e949de432a083a7f246146c5758e357
nav <nramabad@gmail.com>
2022-01-19 15:54:19 -0800

    Fix: experiment key

--- src/js/experiments/languageGuides.js
+++ src/js/experiments/languageGuides.js
@@ -4,7 +4,7 @@ import { displayBlockElement, displayInitialElement } from '../utils';
 // https://app.optimizely.com/v2/projects/16812830475/experiments/20872380274/variations
 export default () =>
   window.OptimizelyClient.getVariationName({
-    experimentKey: 'dd_add_video_tab_to_docs_test',
+    experimentKey: 'dd_guided_language_tours_test',
     groupExperimentName: 'q4_fy22_docs_disco_experiment_group_test',
     experimentContainer: '.treatment',
   })
