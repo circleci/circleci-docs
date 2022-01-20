72c47c0d0f552666044782eee03aa21d54518181
Tony Shen <57234605+tshen815@users.noreply.github.com>
2022-01-20 09:56:46 -0800

    promote to prod and cleanup experiment (#6214)

--- src/js/experiments/index.js
+++ src/js/experiments/index.js
@@ -1,10 +1,8 @@
 import forceAll from './forceAll';
-import surfaceFullExampleConfig from './surfaceFullExampleConfig';
 import videoTutorials from './videoTutorials';
 import './snippetFeedback';
 
 export default {
   forceAll,
-  surfaceFullExampleConfig,
   videoTutorials,
 };
