f347370e4d9a82a9003b1bcd93088bd0c4f89d58
nav <nramabad@gmail.com>
2022-01-24 15:14:52 -0800

    Address: improved comments

--- src/js/experiments/languageGuides.js
+++ src/js/experiments/languageGuides.js
@@ -1,7 +1,7 @@
 import { reconstructToC, highlightTocOnScrollOnce } from '../site/toc';
 import { displayBlockElement, displayInitialElement } from '../utils';
 
-// https://app.optimizely.com/v2/projects/16812830475/experiments/20872380274/variations
+// https://app.optimizely.com/v2/projects/16812830475/experiments/21094250092/variations
 export default () =>
   window.OptimizelyClient.getVariationName({
     experimentKey: 'dd_guided_language_tours_test',
