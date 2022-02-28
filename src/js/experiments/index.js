7d6c8a21ea74a9007bd7c6efa86b1a40c14ef1b7
Tony <tshen815@gmail.com>
2022-02-28 15:13:30 -0600

    implement signup CTA experiment in docs

--- src/js/experiments/index.js
+++ src/js/experiments/index.js
@@ -2,9 +2,11 @@ import forceAll from './forceAll';
 import kbLinks from './kbLinks';
 import languageGuides from './languageGuides';
 import './snippetFeedback';
+import signUpCTA from './signUpCTA';
 
 export default {
   forceAll,
   languageGuides,
   kbLinks,
+  signUpCTA,
 };
---
+++ src/js/experiments/signUpCTA.js
@@ -0,0 +1,14 @@
+// https://app.optimizely.com/v2/projects/16812830475/experiments/21229840508/variations
+window.OptimizelyClient.getVariationName({
+  experimentKey: 'dd_update_signup_cta_test',
+  groupExperimentName: 'q1_fy23_docs_disco_experiment_group_test',
+  experimentContainer: '.global-nav--footer',
+}).then((variation) => {
+  if (variation === 'treatment') {
+    const ctaBtn = document.getElementById('signup-cta');
+    ctaBtn.innerHTML = 'Start Building for Free';
+    ctaBtn.addEventListener('click', () => {
+      window.AnalyticsClient.trackAction('Clicked New Signup CTA');
+    });
+  }
+});
