6796ddb1939084a525f40e844f0358090084411d
teesloane <tylersloane@gmail.com>
2022-03-14 13:50:35 -0400

    Add: trigger popup based on timesVisited.

--- src/js/experiments/popupCta.js
+++ src/js/experiments/popupCta.js
@@ -1,6 +1,11 @@
+const LS_KEY = "timesVisited"
+const SHOW_POPUP_AFTER_N_TIMES = 3
+
 function openPopup() {
   $(".popup-bg").addClass("popup-bg-show");
   $(".popup-content").addClass("popup-content-show");
+
+  window.AnalyticsClient.trackAction('dd_docs-popup-cta_test__popup_seen');
 }
 
 function closePopup() {
@@ -8,26 +13,28 @@ function closePopup() {
   $(".popup-content").removeClass("popup-content-show");
 }
 
+
 /**
- * Sets a counter for timesVisited if it doesn't exist
- * If it does exist, increment it.
- * If timesVisited = , return true
+ * Get the Number of timesVisited from localStorage
+ * if it doesn't exist, create it and set to 1.
  * */
-function handleLocalStorageCounter() {
-  const lsKey = "timesVisited"
-  let timesVisited = localStorage.getItem(lsKey);
-  console.log("times visited is 1", timesVisited);
-  timesVisited = parseInt(timesVisited);
-  console.log("times visited is ", timesVisited);
-  if (isNaN(timesVisited)) { timesVisited = 0; }
-  if (!timesVisited) {
-    localStorage.setItem(lsKey, 0)
-  } else {
-    localStorage.setItem(lsKey, timesVisited += 1)
+function getTimesVisited() {
+  let timesVisited = parseInt(localStorage.getItem(LS_KEY));
+  if (!timesVisited || isNaN(timesVisited)) {
+    timesVisited = 1
+    localStorage.setItem(LS_KEY, timesVisited);
   }
-  return timesVisited
+  return timesVisited;
 }
 
+function incrementTimesVisited() {
+  const timesVisited = getTimesVisited()
+  if (timesVisited == SHOW_POPUP_AFTER_N_TIMES)  {
+    localStorage.setItem(LS_KEY, 0)
+  } else {
+    localStorage.setItem(LS_KEY, timesVisited + 1 )
+  }
+}
 
 // https://app.optimizely.com/v2/projects/16812830475/experiments/21253750305/
 window.OptimizelyClient.getVariationName({
@@ -37,11 +44,12 @@ window.OptimizelyClient.getVariationName({
   // guestExperiment: true, // FIXME: re-enable this for production.
 }).then((variation) => {
   if (variation === 'treatment') {
-
     const popupBg = $(".popup-bg");
     const popupContent = $(".popup-content");
     const skipButton = $(".popup-skip-btn");
-    const timesVisited = handleLocalStorageCounter()
+    const signupButton = $(".popup-signup-cta");
+    const timesVisited = getTimesVisited()
+    console.log(timesVisited);
 
     // REVIEW: are we ok with closing the popup by clicking on the background outside of it
     // of will the user have to click "Skip"?
@@ -52,13 +60,21 @@ window.OptimizelyClient.getVariationName({
       }
     });
 
-    skipButton.click(closePopup)
+    signupButton.click(() => {
+      window.AnalyticsClient.trackAction('dd_docs-popup-cta_test__signup_clicked');
+    })
 
-    if (timesVisited === 3) {
+    skipButton.click(() => {
+      window.AnalyticsClient.trackAction('dd_docs-popup-cta_test__skip_clicked');
+      closePopup()
+    })
+    if (timesVisited === SHOW_POPUP_AFTER_N_TIMES) {
       // don't assume we have a popupwrapper in every docs layout.
       if (popupBg && popupContent) {
         openPopup()
       }
     }
+
+    incrementTimesVisited();
   }
 });
