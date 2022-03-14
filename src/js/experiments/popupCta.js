0843a4f7f062ce30336f8e673806579d24b984f5
teesloane <tylersloane@gmail.com>
2022-03-14 12:08:49 -0400

    Clean: reorg- js.

--- src/js/experiments/popupCta.js
+++ src/js/experiments/popupCta.js
@@ -8,29 +8,57 @@ function closePopup() {
   $(".popup-content").removeClass("popup-content-show");
 }
 
-function init() {
-  let isOpen = false
-  const popupBg = $(".popup-bg");
-  const popupContent = $(".popup-content");
-  const skipButton = $(".popup-skip-btn");
-
-  skipButton.click(() => {
-    closePopup();
-  })
-
-  // REVIEW: are we ok with closing the popup by clicking on the background outside of it
-  // of will the user have to click "Skip"?
-  // if the latter, remove this.
-  popupBg.click(function(event) {
-    if ($(this).has(popupContent).length === 0) {
-      closePopup();
-    }
-  });
-
-  // don't assume we have a popupwrapper in every docs layout.
-  if (popupBg && popupContent) {
-    openPopup()
+/**
+ * Sets a counter for timesVisited if it doesn't exist
+ * If it does exist, increment it.
+ * If timesVisited = , return true
+ * */
+function handleLocalStorageCounter() {
+  const lsKey = "timesVisited"
+  let timesVisited = localStorage.getItem(lsKey);
+  console.log("times visited is 1", timesVisited);
+  timesVisited = parseInt(timesVisited);
+  console.log("times visited is ", timesVisited);
+  if (isNaN(timesVisited)) { timesVisited = 0; }
+  if (!timesVisited) {
+    localStorage.setItem(lsKey, 0)
+  } else {
+    localStorage.setItem(lsKey, timesVisited += 1)
   }
+  return timesVisited
 }
 
-init();
+
+// https://app.optimizely.com/v2/projects/16812830475/experiments/21253750305/
+window.OptimizelyClient.getVariationName({
+  experimentKey: 'dd_docs-popup-cta_test',
+  groupExperimentName: 'q1_fy23_docs_disco_experiment_group_test',
+  experimentContainer: '.main-body',
+  // guestExperiment: true, // FIXME: re-enable this for production.
+}).then((variation) => {
+  if (variation === 'treatment') {
+
+    const popupBg = $(".popup-bg");
+    const popupContent = $(".popup-content");
+    const skipButton = $(".popup-skip-btn");
+    const timesVisited = handleLocalStorageCounter()
+
+    // REVIEW: are we ok with closing the popup by clicking on the background outside of it
+    // of will the user have to click "Skip"?
+    // if the latter, remove this.
+    popupBg.click(function(event) {
+      if ($(this).has(popupContent).length === 0) {
+        closePopup();
+      }
+    });
+
+    skipButton.click(closePopup)
+
+    if (timesVisited === 3) {
+      // don't assume we have a popupwrapper in every docs layout.
+      if (popupBg && popupContent) {
+        openPopup()
+      }
+    }
+  }
+});
