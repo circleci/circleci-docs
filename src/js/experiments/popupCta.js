fc0041dfd90642b42491b4f6d5d1cedcfe34fb37
teesloane <tylersloane@gmail.com>
2022-03-14 10:37:57 -0400

    Add: possible close-by-clicking-outside modal.
    pending review.

--- src/js/experiments/popupCta.js
+++ src/js/experiments/popupCta.js
@@ -18,6 +18,15 @@ function init() {
     closePopup();
   })
 
+  // REVIEW: are we ok with closing the popup by clicking on the background outside of it
+  // of will the user have to click "Skip"?
+  // if the latter, remove this.
+  popupBg.click(function(event) {
+    if ($(this).has(popupContent).length === 0) {
+      closePopup();
+    }
+  });
+
   // don't assume we have a popupwrapper in every docs layout.
   if (popupBg && popupContent) {
     openPopup()
