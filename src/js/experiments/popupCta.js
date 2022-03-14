f28b897c34a5748cbb191c4e0aaf0a6c65cb7c03
teesloane <tylersloane@gmail.com>
2022-03-14 15:26:42 -0400

    Fix: linting.

--- src/js/experiments/popupCta.js
+++ src/js/experiments/popupCta.js
@@ -1,21 +1,20 @@
-const LS_KEY = "timesVisited"
-const SHOW_POPUP_AFTER_N_TIMES = 3
+const LS_KEY = 'timesVisited';
+const SHOW_POPUP_AFTER_N_TIMES = 3;
 
 function openPopup() {
-  $(".popup-bg").addClass("popup-bg-show");
-  $(".popup-content").addClass("popup-content-show");
+  $('.popup-bg').addClass('popup-bg-show');
+  $('.popup-content').addClass('popup-content-show');
 
   window.AnalyticsClient.trackAction('dd_docs-popup-cta_test__popup_seen');
 }
 
 function closePopup(trackIt = true) {
-  $(".popup-bg").removeClass("popup-bg-show");
-  $(".popup-content").removeClass("popup-content-show");
-  if(trackIt) {
+  $('.popup-bg').removeClass('popup-bg-show');
+  $('.popup-content').removeClass('popup-content-show');
+  if (trackIt) {
     window.AnalyticsClient.trackAction('dd_docs-popup-cta_test__popup_closed');
   }
 }
-
 
 /**
  * Get the Number of timesVisited from localStorage
@@ -24,18 +23,18 @@ function closePopup(trackIt = true) {
 function getTimesVisited() {
   let timesVisited = parseInt(localStorage.getItem(LS_KEY));
   if (!timesVisited || isNaN(timesVisited)) {
-    timesVisited = 1
+    timesVisited = 1;
     localStorage.setItem(LS_KEY, timesVisited);
   }
   return timesVisited;
 }
 
 function incrementTimesVisited() {
-  const timesVisited = getTimesVisited()
-  if (timesVisited == SHOW_POPUP_AFTER_N_TIMES)  {
-    localStorage.setItem(LS_KEY, 0)
+  const timesVisited = getTimesVisited();
+  if (timesVisited == SHOW_POPUP_AFTER_N_TIMES) {
+    localStorage.setItem(LS_KEY, 0);
   } else {
-    localStorage.setItem(LS_KEY, timesVisited + 1 )
+    localStorage.setItem(LS_KEY, timesVisited + 1);
   }
 }
 
@@ -44,32 +43,34 @@ window.OptimizelyClient.getVariationName({
   experimentKey: 'dd_docs-popup-cta_test',
   groupExperimentName: 'q1_fy23_docs_disco_experiment_group_test',
   experimentContainer: '.main-body',
-  guestExperiment: true
+  guestExperiment: true,
 }).then((variation) => {
   if (variation === 'treatment') {
-    const popupBg = $(".popup-bg");
-    const popupContent = $(".popup-content");
-    const skipButton = $(".popup-skip-btn");
-    const signupButton = $(".popup-signup-cta");
-    const timesVisited = getTimesVisited()
-    popupBg.click(function(event) {
+    const popupBg = $('.popup-bg');
+    const popupContent = $('.popup-content');
+    const skipButton = $('.popup-skip-btn');
+    const signupButton = $('.popup-signup-cta');
+    const timesVisited = getTimesVisited();
+    popupBg.click(function () {
       if ($(this).has(popupContent).length === 0) {
         closePopup();
       }
     });
 
     signupButton.click(() => {
-      window.AnalyticsClient.trackAction('dd_docs-popup-cta_test__signup_clicked');
+      window.AnalyticsClient.trackAction(
+        'dd_docs-popup-cta_test__signup_clicked',
+      );
       closePopup(false);
-    })
+    });
 
     skipButton.click(() => {
       closePopup();
-    })
+    });
     if (timesVisited === SHOW_POPUP_AFTER_N_TIMES) {
       // don't assume we have a popupwrapper in every docs layout.
       if (popupBg && popupContent) {
-        openPopup()
+        openPopup();
       }
     }
 
