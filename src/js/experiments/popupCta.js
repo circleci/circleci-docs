6452665991cc5231a79e733cbb21b85eb7da5e2b
teesloane <tylersloane@gmail.com>
2022-03-15 17:17:08 -0400

    Add: only show to guest users
    - refactor local storage to store last time popup was seen.

--- src/js/experiments/popupCta.js
+++ src/js/experiments/popupCta.js
@@ -1,4 +1,4 @@
-const LS_KEY = 'timesVisited';
+const LS_KEY = 'cciPopupData';
 const SHOW_POPUP_AFTER_N_TIMES = 3;
 
 function openPopup() {
@@ -20,22 +20,49 @@ function closePopup(trackIt = true) {
  * Get the Number of timesVisited from localStorage
  * if it doesn't exist, create it and set to 1.
  * */
-function getTimesVisited() {
-  let timesVisited = parseInt(localStorage.getItem(LS_KEY));
-  if (!timesVisited || isNaN(timesVisited)) {
-    timesVisited = 1;
-    localStorage.setItem(LS_KEY, timesVisited);
+function getPopupData() {
+  if (!localStorage.getItem(LS_KEY)) {
+    localStorage.setItem(LS_KEY, JSON.stringify({
+      timesVisited: 1,
+      lastSeen: null
+    }))
   }
-  return timesVisited;
+  let popupData = JSON.parse(localStorage.getItem(LS_KEY));
+  return popupData;
 }
 
-function incrementTimesVisited() {
-  const timesVisited = getTimesVisited();
-  if (timesVisited === SHOW_POPUP_AFTER_N_TIMES) {
-    localStorage.setItem(LS_KEY, 0);
-  } else {
-    localStorage.setItem(LS_KEY, timesVisited + 1);
+/**
+ * In order to show the popup to users we need to:
+ * a) the user has to have visited three pages ("timesVisited")
+ * b) the user must not have already seen a popup today.
+ *
+ * In this function we pull the values around this from local storage, do some checks
+ * and determine if we can show the popup.
+ * */
+function canShowPopup() {
+  let now = new Date();
+  let stillTheSameDay = false
+  const popupData = getPopupData();
+  if(popupData.lastSeen !== null) {
+    const popupLastSeen = new Date(popupData.lastSeen);
+    stillTheSameDay =
+      now.getFullYear() === popupLastSeen.getFullYear() &&
+      now.getMonth() === popupLastSeen.getMonth() &&
+      now.getDate() === popupLastSeen.getDate()
   }
+  return popupData.timesVisited === SHOW_POPUP_AFTER_N_TIMES && !stillTheSameDay
+}
+
+
+function incrementTimesVisited() {
+  let popupData = getPopupData();
+  if (popupData.timesVisited === SHOW_POPUP_AFTER_N_TIMES) {
+    popupData.timesVisited = 0
+    popupData.lastSeen = new Date()
+  } else {
+    popupData.timesVisited += 1
+  }
+  localStorage.setItem(LS_KEY, JSON.stringify(popupData));
 }
 
 // https://app.optimizely.com/v2/projects/16812830475/experiments/21253750305/
@@ -43,6 +70,10 @@ window.OptimizelyClient.getVariationName({
   experimentKey: 'dd_docs-popup-cta_test',
   groupExperimentName: 'q1_fy23_docs_disco_experiment_group_test',
   experimentContainer: '.main-body',
+  attributes: {
+    // This will only show the experiment to people who are guests to the docs site.
+    docs_is_logged_in: window.userData?.created_at === undefined
+  },
   guestExperiment: true,
 }).then((variation) => {
   if (variation === 'treatment') {
@@ -50,7 +81,7 @@ window.OptimizelyClient.getVariationName({
     const popupContent = $('.popup-content');
     const skipButton = $('.popup-skip-btn');
     const signupButton = $('.popup-signup-cta');
-    const timesVisited = getTimesVisited();
+
     popupBg.click(function () {
       if ($(this).has(popupContent).length === 0) {
         closePopup();
@@ -67,7 +98,7 @@ window.OptimizelyClient.getVariationName({
     skipButton.click(() => {
       closePopup();
     });
-    if (timesVisited === SHOW_POPUP_AFTER_N_TIMES) {
+    if (canShowPopup()) {
       // don't assume we have a popupwrapper in every docs layout.
       if (popupBg && popupContent) {
         openPopup();
