05e18831d2c9461f4295a1b6f6a8724eff411379
tees <12987958+teesloane@users.noreply.github.com>
2022-03-29 12:00:28 -0400

    [DD-454] - Add "new" badge to sidebar for Quickstart Guide (#6591)
    * Add: "new" badge to sidebar.
    
    * Add: new badge as css rather than svg.
    
    * add: ternary for active class for getting-started.
    
    * Add: lint fixes.
    
    * Fix: experiment only needs to work in EN
    
    * linting

--- src/js/experiments/gettingStartedGuide.js
+++ src/js/experiments/gettingStartedGuide.js
@@ -1,5 +1,26 @@
 // https://app.optimizely.com/v2/projects/16812830475/experiments/21268663100/variations
 
+/**
+ * Changes "Your First Green Build" in the docs sidebar to "Quickstart Guide"
+ * Adds a "NEW" badge to the sidebar item as well.
+ **/
+function addNewBadgeToSidebar() {
+  const isGettingStartedPage =
+    window.location.pathname == '/docs/2.0/getting-started/';
+
+  const NEW_SIDEBAR_HTML = `
+    <a class="${isGettingStartedPage ? 'active' : ''}"
+       style="display: flex; align-items: center"
+       href="/docs/2.0/getting-started/"
+       data-section="getting-started" data-proofer-ignore="">
+      <span>Quickstart Guide</span>
+      <span class="getting-started-new-badge"> NEW </span>
+    </a>
+`;
+
+  $("li > a[href='/docs/2.0/getting-started/']").replaceWith(NEW_SIDEBAR_HTML);
+}
+
 // NOTE: experimentContainer can be updated once we have the other components of this experiment created and in place
 window.OptimizelyClient.getVariationName({
   experimentKey: 'dd_getting_started_docs_test',
@@ -8,6 +29,7 @@ window.OptimizelyClient.getVariationName({
   guestExperiment: false,
 }).then((variation) => {
   if (variation === 'treatment') {
+    addNewBadgeToSidebar();
     // console.log('IN TREATMENT GROUP');
   }
   if (variation === 'control') {
