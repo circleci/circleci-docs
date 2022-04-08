d88b110e38a59a2ed743682548a9ccd8662a8a58
Tony <tshen815@gmail.com>
2022-04-07 21:31:44 -0500

    implement experiment logic

--- src/js/experiments/gettingStartedGuide.js
+++ src/js/experiments/gettingStartedGuide.js
@@ -1,9 +1,5 @@
 // https://app.optimizely.com/v2/projects/16812830475/experiments/21268663100/variations
 
-/**
- * Changes "Your First Green Build" in the docs sidebar to "Quickstart Guide"
- * Adds a "NEW" badge to the sidebar item as well.
- **/
 function addNewBadgeToSidebar() {
   const isGettingStartedPage =
     window.location.pathname == '/docs/2.0/getting-started/';
@@ -29,7 +25,6 @@ function showHomePageBadges() {
   }
 }
 
-// NOTE: experimentContainer can be updated once we have the other components of this experiment created and in place
 window.OptimizelyClient.getVariationName({
   experimentKey: 'dd_getting_started_docs_test',
   groupExperimentName: 'q1_fy23_docs_disco_experiment_group_test',
@@ -37,7 +32,52 @@ window.OptimizelyClient.getVariationName({
   guestExperiment: false,
 }).then((variation) => {
   if (variation === 'treatment') {
+    // Used to expand the container width for the content
+    if (window.location.pathname === '/docs/2.0/getting-started/') {
+      const articleContainer = $('.quickstart-guide');
+      articleContainer.addClass('getting-started-full-width');
+    }
+
+    // Init new badge in sidebar and add tracking event
     addNewBadgeToSidebar();
+
+    // Init new badge on landing page and add tracking events
     showHomePageBadges();
+    const badges = Array.from($('.wrapper-link'));
+    badges.forEach((badge) => {
+      badge.addEventListener('click', () => {
+        window.AnalyticsClient.trackAction('clicked-landing-page-badge', {
+          badgeText: badge.innerText,
+          badgeHref: badge.href,
+        });
+      });
+    });
+
+    // Display content on page if users in treatment
+    const treatment = $('.treatment');
+    treatment.css('display', 'block');
+
+    // Add tracking to all links on page
+    const links = Array.from($('.treatment').find($('a')));
+    links.forEach((link, i) => {
+      link.addEventListener('click', () => {
+        window.AnalyticsClient.trackAction(
+          'clicked-on-getting-started-guide-link',
+          {
+            linkText: link.innerText,
+            linkHref: link.href,
+            linkIndexOnPage: i + 1 + '/20',
+            page: window.location.pathname,
+          },
+        );
+      });
+    });
+  }
+  if (variation === 'control') {
+    const control = $('.control');
+    control.css('display', 'block');
+    // ToC is hidden due to using getting-started-guide-experimental for the layout, setting the css to ensure that the ToC is present in control variation
+    const toc = $('#full-height');
+    toc.css('visibility', 'visible');
   }
 });
