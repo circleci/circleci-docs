5f93c3635ca948454d793859514021a06c329bd7
Tony Shen <57234605+tshen815@users.noreply.github.com>
2022-04-13 14:24:21 -0700

    [DD-545] Permanent Push: Update the Sign-up CTA button verbiage (#6688)
    * remove experiment logic from codebase
    
    * make new CTA permanent

--- src/js/experiments/index.js
+++ src/js/experiments/index.js
@@ -2,14 +2,12 @@ import forceAll from './forceAll';
 import languageGuides from './languageGuides';
 import signupLinksCta from './signupLinksCta';
 import './snippetFeedback';
-import signUpCTA from './signUpCTA';
 import troubleShootingCommonIssues from './troubleshootingCommonIssues';
 import popupCta from './popupCta';
 
 export default {
   forceAll,
   languageGuides,
-  signUpCTA,
   signupLinksCta,
   troubleShootingCommonIssues,
   popupCta,
