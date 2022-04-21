9cc62e1b43e586a5e5d45885c164882a2b331ccd
Tony Shen <57234605+tshen815@users.noreply.github.com>
2022-04-21 09:08:28 -0700

    [DD-450] Getting Started Experiment Boilerplate (#6544)
    * initial experiment set up
    
    * add comments for clarity
    
    * import experiment to app
    
    * [DD-454] - Add "new" badge to sidebar for Quickstart Guide (#6591)
    
    * Add: "new" badge to sidebar.
    
    * Add: new badge as css rather than svg.
    
    * add: ternary for active class for getting-started.
    
    * Add: lint fixes.
    
    * Fix: experiment only needs to work in EN
    
    * linting
    
    * [DD-454] - Add "new" badge to sidebar for Quickstart Guide (#6591)
    
    * Add: "new" badge to sidebar.
    
    * Add: new badge as css rather than svg.
    
    * add: ternary for active class for getting-started.
    
    * Add: lint fixes.
    
    * Fix: experiment only needs to work in EN
    
    * linting
    
    * [Dd 455] - homepage redesign preview (#6644)
    
    * Start: homepage redesign includes.
    
    * Fix mixin failures
    
    * Add: logos and cleanup padding.
    
    * Add: getting-started badges and experiment code.
    
    * Fix: linting.
    
    * fix: spacing.
    
    * Review: Make suggested style changes.
    
    * Fix - suggested changes.
    
    * [DD-456] Finalize Getting Started Experiment (#6650)
    
    * initial experiment set up
    
    * add comments for clarity
    
    * import experiment to app
    
    * [DD-454] - Add "new" badge to sidebar for Quickstart Guide (#6591)
    
    * Add: "new" badge to sidebar.
    
    * Add: new badge as css rather than svg.
    
    * add: ternary for active class for getting-started.
    
    * Add: lint fixes.
    
    * Fix: experiment only needs to work in EN
    
    * linting
    
    * initial experiment set up
    
    * add comments for clarity
    
    * import experiment to app
    
    * [DD-454] - Add "new" badge to sidebar for Quickstart Guide (#6591)
    
    * Add: "new" badge to sidebar.
    
    * Add: new badge as css rather than svg.
    
    * add: ternary for active class for getting-started.
    
    * Add: lint fixes.
    
    * Fix: experiment only needs to work in EN
    
    * linting
    
    * test png image for two up content
    
    * [Dd 455] - homepage redesign preview (#6644)
    
    * Start: homepage redesign includes.
    
    * Fix mixin failures
    
    * Add: logos and cleanup padding.
    
    * Add: getting-started badges and experiment code.
    
    * Fix: linting.
    
    * fix: spacing.
    
    * Review: Make suggested style changes.
    
    * Fix - suggested changes.
    
    * import images for experiment into getting-started-guide-exp folder
    
    * set font color
    
    * implement styles and html includes for prereq section
    
    * add spacing
    
    * make header includes dynamic to repurpose throughout page
    
    * experiment page for getting started guide
    
    * add top space for prereq section
    
    * refactor includes to make href and content dynamic
    
    * add dynamic getting-started-links includes
    
    * fix typo in class name
    
    * remove comments
    
    * display a tags based on usage for prereq or resources section
    
    * add comments for flarity
    
    * use experiment layout to display content for treatment variation
    
    * adjust content for .md files for getting started guide
    
    * add class to set width for experiment content
    
    * implement experiment logic
    
    * remove comments from md file
    
    * appease CCI translation bot
    
    * update linkIndexOnPage property data
    
    * refactor how we are initializing tracking for links and badges
    
    * update comments
    
    Co-authored-by: tees <12987958+teesloane@users.noreply.github.com>
    
    * remove old badge design for landing page
    
    * ensure something is displayed all the time
    
    * update spacing of getting started experiment
    
    * title should be Your First Green Build in control
    
    * fix footer
    
    * add reconstructToC
    
    * fix title name not match
    
    * add track action docs-getting-started-observed
    
    * Add comment
    
    * update articleContainer class to display full width of experiment
    
    * refactor experiment logic into reusable functions
    
    * fix when run treatment only when on right url
    
    Co-authored-by: tees <12987958+teesloane@users.noreply.github.com>
    Co-authored-by: chantelle g <chantelle@circleci.com>

--- src/js/experiments/index.js
+++ src/js/experiments/index.js
@@ -2,10 +2,12 @@ import forceAll from './forceAll';
 import languageGuides from './languageGuides';
 import troubleShootingCommonIssues from './troubleshootingCommonIssues';
 import popupCta from './popupCta';
+import gettingStartedGuide from './gettingStartedGuide';
 
 export default {
   forceAll,
   languageGuides,
   troubleShootingCommonIssues,
   popupCta,
+  gettingStartedGuide,
 };
