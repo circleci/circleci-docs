7beffeb39547a4378c98f3910b7f790bb384f08a
tees <12987958+teesloane@users.noreply.github.com>
2022-04-21 07:46:54 -0700

    [DD-539] Clean: tear down snippetFeedback experiment. (#6690)
    * rebase
    
    * Fix: lint.
    
    * Fix: lint.
    
    Co-authored-by: Tony <tshen815@gmail.com>

--- src/js/experiments/index.js
+++ src/js/experiments/index.js
@@ -1,6 +1,5 @@
 import forceAll from './forceAll';
 import languageGuides from './languageGuides';
-import './snippetFeedback';
 import troubleShootingCommonIssues from './troubleshootingCommonIssues';
 import popupCta from './popupCta';
 
