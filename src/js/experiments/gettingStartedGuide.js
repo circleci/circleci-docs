95a38b87ce3d9682fad69977fe49f681950f107a
Tony <tshen815@gmail.com>
2022-04-28 18:03:46 -0700

    implement trackAction for sidebar link

--- src/js/experiments/gettingStartedGuide.js
+++ src/js/experiments/gettingStartedGuide.js
@@ -5,11 +5,12 @@ function addNewBadgeToSidebar() {
   const isGettingStartedPage =
     window.location.pathname == '/docs/2.0/getting-started/';
   const NEW_SIDEBAR_HTML = `
-    <a class="${isGettingStartedPage ? 'active' : ''}"
+    <a id="getting-started-guide" class="${isGettingStartedPage ? 'active' : ''
+    }"
        style="display: flex; align-items: center"
        href="/docs/2.0/getting-started/"
        data-section="getting-started" data-proofer-ignore="">
-      <span>Quickstart Guide</span>
+      <span id="getting-started-guide">Quickstart Guide</span>
       <span class="getting-started-new-badge"> NEW </span>
     </a>
 `;
@@ -23,30 +24,44 @@ function showHomePageBadges() {
   }
 }
 
-function setUpTracking() {
-  const badges = Array.from($('.wrapper-link'));
-  badges.forEach((badge) => {
-    badge.addEventListener('click', () => {
-      window.AnalyticsClient.trackAction('clicked-landing-page-badge', {
-        badgeText: badge.innerText,
-        badgeHref: badge.href,
+function setUpTracking(variation) {
+  const newBadges = $("li > a[href='/docs/2.0/getting-started/']");
+  console.log(newBadges);
+  if (newBadges) {
+    const sidebarBadge = newBadges[1];
+    sidebarBadge.addEventListener('click', () => {
+      window.AnalyticsClient.trackAction('clicked-sidebar-getting-started', {
+        variation,
       });
     });
-  });
-  const links = Array.from($('.treatment').find($('a')));
-  links.forEach((link, i) => {
-    link.addEventListener('click', () => {
-      window.AnalyticsClient.trackAction(
-        'clicked-on-getting-started-guide-link',
-        {
-          linkText: link.innerText,
-          linkHref: link.href,
-          linkIndexOnPage: i + 1 + '/' + links.length,
-          page: window.location.pathname,
-        },
-      );
+  }
+
+  // Since we are using this function for both variations, we only want to add tracking for elements if we are in treatment as they are unique for the experiment
+  if (variation === 'treatment') {
+    const badges = Array.from($('.wrapper-link'));
+    badges.forEach((badge) => {
+      badge.addEventListener('click', () => {
+        window.AnalyticsClient.trackAction('clicked-landing-page-badge', {
+          badgeText: badge.innerText,
+          badgeHref: badge.href,
+        });
+      });
     });
-  });
+
+    const links = Array.from($('.treatment').find($('a')));
+    links.forEach((link, i) => {
+      link.addEventListener('click', () => {
+        window.AnalyticsClient.trackAction(
+          'clicked-on-getting-started-guide-link',
+          {
+            linkText: link.innerText,
+            linkIndexOnPage: i + 1 + '/' + links.length,
+            page: window.location.pathname,
+          },
+        );
+      });
+    });
+  }
 }
 
 function displayGettingStartedContent() {
@@ -93,21 +108,24 @@ window.OptimizelyClient.getVariationName({
 })
   .then((variation) => {
     if (variation === 'treatment') {
-      // Init new badges on landing page
-      showHomePageBadges();
-
       // Init NEW badge in sidebar
       addNewBadgeToSidebar();
+
+      // Init new badges on landing page
+      showHomePageBadges();
 
       // Display all content and layout for Quickstart Guide
       displayGettingStartedContent();
 
       // Init tracking for experiment links and landing page badges
-      setUpTracking();
+      setUpTracking(variation);
     } else {
       displayFirstGreenBuildContent();
+      setUpTracking(variation);
     }
   })
   .catch(() => {
+    // In case if users have ad blocker on which blocks optimizely, we will show content for control variation
     displayFirstGreenBuildContent();
+    setUpTracking('control');
   });
