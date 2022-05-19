6320949b4810daaf8827d83cc2f15789077f641b
rsylvian <romain.sylvian@gmail.com>
2022-05-18 23:58:09 -0700

    add slow rollout

---
+++ src/js/experiments/docsDetangling.js
@@ -0,0 +1,46 @@
+// Because these pages have experiments we don't want to redirect users to docs-platform for now
+const pagesWithExperiments = [
+  '/docs/',
+  '/docs/2.0/getting-started/',
+  '/docs/2.0/first-steps/',
+  '/docs/2.0/troubleshooting-common-issues/',
+  '/docs/2.0/language-python/',
+  '/docs/2.0/language-javascript/',
+  '/docs/ja/',
+  '/docs/ja/2.0/getting-started/',
+  '/docs/ja/2.0/first-steps/',
+  '/docs/ja/2.0/troubleshooting-common-issues/',
+  '/docs/ja/2.0/language-python/',
+  '/docs/ja/2.0/language-javascript/',
+];
+
+console.log(pagesWithExperiments.includes(window.location.pathname));
+
+window.OptimizelyClient.getVariationName({
+  experimentKey: 'dd_docs_detangling_redirect_test',
+  groupExperimentName: 'q2_fy23_docs_disco_experiment_group_test',
+  experimentContainer: 'body',
+  guestExperiment: true,
+  attributes: {
+    is_page_docs_disco_experiment: pagesWithExperiments.includes(
+      window.location.pathname,
+    ),
+  },
+}).then((variation) => {
+  console.log(variation);
+
+  if (variation === 'treatment') {
+    const currentUrl = window.location.href;
+
+    window.AnalyticsClient.trackAction('dd_docs-detangling-redirect', {
+      url: currentUrl,
+      page: window.location.pathname,
+    });
+
+    // redirect to docs-platform
+    window.location.href = currentUrl.replace(
+      'circleci.com/docs',
+      'circleci.com/docs2',
+    );
+  }
+});
