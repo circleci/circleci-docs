fc7a30b86c508f7aad4cbed46890f2e193d5c9b7
rsylvian <romain.sylvian@gmail.com>
2022-06-23 18:10:41 -0700

    bring back some of the submodule

--- src/js/site/index.js
+++ src/js/site/index.js
@@ -1,7 +1,7 @@
 import * as main from './main';
 //import * as nav from './nav';
-//import * as sidebar from './sidebar';
+import * as sidebar from './sidebar';
 import * as toc from './toc';
-import * as user from './user';
+//import * as user from './user';
 
-export default { main, toc, user };
+export default { main, sidebar, toc };
