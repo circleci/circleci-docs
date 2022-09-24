ed976db051396598f89848c528c0e5c9e726a3f3
chantelle g <chantelle@circleci.com>
2022-09-23 21:31:47 -0400

    update script todo: comments

--- scripts/frontmatter_tagging/frontmatter_tagging.py
+++ scripts/frontmatter_tagging/frontmatter_tagging.py
@@ -2,13 +2,11 @@
 
 # This script iterates over our docs and converts version front matter to tags format
 # sectionTags are not dealt with in this script
-
 import os
 import glob
 import sys
-import frontmatter
 from pathlib import Path
-from editfrontmatter import EditFrontMatter
+import re
 
 ## Select desired markdown and adoc files
 docs_paths = (
@@ -30,34 +28,28 @@ def iterate_docs():
 
     for file in files:
       path = Path(file)
-      post = frontmatter.load(path)
-      keys = post.keys()
-      
-      version = None ### Test case version needs to be reset to not be version from previous run
-      if post.get('version') is not None: ### Test case version will be deleted need to store it
-        version = post['version']
-
       if not os.path.exists('tmp/' + str(path.parent.name)): ## Test case file directory could expand e.g _cci2_ko
         os.mkdir('tmp/' + str(path.parent.name))
 
       # location to store new versions of each file
       tmpPath = 'tmp/' + str(path.parent.name) + '/'+ str(Path(path).stem) + str(Path(path).suffix)
+    
+      f = open(file, "r")
 
-      if version is not None: ### Test case file has version frontmatter
-        template_str = ''.join(open(os.path.abspath("frontmatter_template_tags.j2"), "r").readlines())
-        proc = EditFrontMatter(file_path=path, template_str=template_str)
-        proc.keys_toDelete = ['version']
-        proc.run()
-        proc.run({'addedVariable': version})
-        with open(tmpPath, 'w') as f:
-          print(proc.dumpFileData(), file=f)
-        ### Test case file has no front matter and file does not have version frontmatter
-        ### Covered inside of _config default value null
+      content = f.read()
 
-        # This is a work around of EditFrontMatter not encoding japanese characters properly
-        # We rewrite the file with frontmatter that does encode japanese correctly
-        post = frontmatter.load(tmpPath)
-        with open(tmpPath, 'w') as f:
-          print(frontmatter.dumps(post), file=f)
+      matchFrontmatter = re.search(r'^---[\s\S]+?---', content)
+
+      if matchFrontmatter:
+        if matchFrontmatter[0].find("version:") != -1:
+          temp = open(tmpPath, "w")
+          matchVersion = re.search( r'version:[\s\S]+?---', content)[0]
+          removeFirstnLast = matchVersion.replace('\n', '\n' + "  ")
+          indentedChildren = removeFirstnLast[removeFirstnLast.find('\n')+1:removeFirstnLast.rfind('\n')]
+          newContent = content.replace(matchVersion[matchVersion.find('\n')+1:matchVersion.rfind('\n')], indentedChildren)
+          newFrontmatter = re.search( r'^---[\s\S]+?---', newContent)[0]
+          contentTagsFrontmatter = newFrontmatter.replace('version:','contentTags: \n  platform:')
+          replacedoriginal = newContent.replace(newFrontmatter, contentTagsFrontmatter)
+          temp.write(replacedoriginal)
 
 iterate_docs()
