ea820158c060d21221639568df3feb5c3b1b1eb3
J Meredith <5113432+j2em6h@users.noreply.github.com>
2025-07-14 11:54:19 -0400

    Add Cookiebot (#108)
    Use auto blockingmode
    Set cbid production value in antora playbook, but allow to be over
    -written by a local env variable for testing with staging value

--- gulp.d/tasks/build-site.js
+++ gulp.d/tasks/build-site.js
@@ -9,7 +9,7 @@ console.log('Resolved path:', resolvedPath);
 
 module.exports = function buildSite(cb) {
   console.log('Starting Antora build...')
-  exec(`npx antora ${antoraPlaybook} --log-failure-level=warn --extension ${resolvedPath} --extension ${resolvedPathExportExtension} --key segment_write=${process.env.SEGMENT_WRITE_KEY || null}`, (err, stdout, stderr) => {
+  exec(`npx antora ${antoraPlaybook} --log-failure-level=warn --extension ${resolvedPath} --extension ${resolvedPathExportExtension} --key segment_write=${process.env.SEGMENT_WRITE_KEY || null} ${process.env.COOKIEBOT ? `--key cookiebot=${process.env.COOKIEBOT}` : ''}`, (err, stdout, stderr) => {
     console.log(stdout)
     if (stderr) console.error(stderr)
     if (err) return cb(err)
