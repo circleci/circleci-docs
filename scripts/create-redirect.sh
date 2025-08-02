f4fefec2f616e905292f7b589ec8456cd0219240
meeech <4623+meeech@users.noreply.github.com>
2025-08-02 11:05:23 +0100

    fix bad redirects

--- scripts/create-redirect.sh
+++ scripts/create-redirect.sh
@@ -8,15 +8,17 @@ if [ "$#" -ne 3 ]; then
   exit 1
 fi
 
+DESTINATION_FOLDER="docs-preview"
+
 BUCKET_NAME="$1"
 OLD_PATH="$2"   # must include leading and trailing slash e.g. /about-circleci/
-NEW_PATH="$3"   # full target path e.g. /docs-preview/guide/about-circleci/index.html
+NEW_PATH="/${DESTINATION_FOLDER}$3"   # full target path e.g. /docs-preview/guide/about-circleci/index.html
 
 echo "[INFO] Deploying redirect to bucket: s3://$BUCKET_NAME"
 echo "[INFO] Redirect: $OLD_PATH -> $NEW_PATH"
 
 # Convert old path to S3 key (no leading slash before key!)
-S3_KEY="docs-preview${OLD_PATH}index.html"
+S3_KEY="${DESTINATION_FOLDER}${OLD_PATH}index.html"
 
 echo "[INFO] Creating S3 object: $S3_KEY"
 
